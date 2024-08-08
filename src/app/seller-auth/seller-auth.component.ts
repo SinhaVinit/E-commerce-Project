4a816099d9308d940835e75dff974f647449ba5a
SinhaVinit <vinit.8444935532@gmail.com>
2024-08-08 11:59:02 +0530

    Integrated Seller SignUp API

--- src/app/seller-auth/seller-auth.component.ts
+++ src/app/seller-auth/seller-auth.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { SellerService } from '../services/seller.service';
+import { Router } from '@angular/router';
+import { SignUp } from '../data-type';
 
 @Component({
   selector: 'app-seller-auth',
@@ -7,8 +10,16 @@ import { Component } from '@angular/core';
 })
 export class SellerAuthComponent {
 
-  signUp(data: object): void {
-    console.log(data);
+  constructor(private seller:SellerService, private router:Router) {}
+
+  signUp(data: SignUp): void {
+    // console.log(data);
+    this.seller.userSignUp(data).subscribe((result: any) => {
+      // console.log(result);
+      if(result) {
+        this.router.navigate(['seller-home']);
+      }
+    });
   }
 
 }
